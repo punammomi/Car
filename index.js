71b26ca4a951b7a7f59821ba7bf2baacc6a9bce4
Punam <159411425+punammomi@users.noreply.github.com>
2026-02-19 20:10:32 +0600

    doing sum using function

--- index.js
+++ index.js
@@ -7,3 +7,14 @@ function countdown(){
     console.log("Blastoff!")}
 
     countdown()
+
+    let lab1=34
+    let lab2=45
+    let lab3=56
+
+    function loglabTime(){
+        let counter=lab1+lab2+lab3
+        console.log(counter)
+    }
+    loglabTime()
+   
